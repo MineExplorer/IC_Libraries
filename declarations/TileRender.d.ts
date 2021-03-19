909d5ae72adfa7e2723ab1070658fdd33f909db6
Vladislav <vladgrz40@gmail.com>
2021-03-19 04:45:25 +0300

    Update TileRender.d.ts

--- declarations/TileRender.d.ts
+++ declarations/TileRender.d.ts
@@ -13,18 +13,19 @@ declare namespace TileRenderer {
     function setStandardModelWithRotation(id: number, data: number, texture: [string, number][], hasVertical?: boolean): void;
     /** @deprecated use setStandardModel instead*/
     function setStandartModel(id: number, texture: [string, number][], data?: number): void;
+    function setHandAndUiModel(id: number, data: number, texture: [string, number][]): void;
     function registerRenderModel(id: number, data: number, texture: [string, number][]): void;
     function registerModelWithRotation(id: number, data: number, texture: [string, number][], hasVertical?: boolean): void;
     /** @deprecated use registerModelWithRotation instead*/
     function registerRotationModel(id: number, data: number, texture: [string, number][]): void;
     /** @deprecated use registerModelWithRotation instead*/
     function registerFullRotationModel(id: number, data: number, texture: [string, number][]): void;
-    function getRenderModel(id: number, data: number | string): ICRender.Model;
-    function mapAtCoords(x: number, y: number, z: number, id: number, data: number | string): void;
-    function getBlockRotation(hasVertical?: boolean): number;
-    function setRotationFunction(id: number, hasVertical?: boolean, placeSound?: string): void;
+    function getRenderModel(id: number, data: string | number): ICRender.Model;
+    function mapAtCoords(x: number, y: number, z: number, id: number, data: string | number): void;
+    function getBlockRotation(player: number, hasVertical?: boolean): number;
+    function setRotationFunction(id: string | number, hasVertical?: boolean, placeSound?: string): void;
     /** @deprecated use setRotationFunction instead*/
-    function setRotationPlaceFunction(id: number, hasVertical?: boolean, placeSound?: string): void;
+    function setRotationPlaceFunction(id: string | number, hasVertical?: boolean, placeSound?: string): void;
     function setupWireModel(id: number, data: number, width: number, groupName: string, preventSelfAdd?: boolean): void;
     function getCropModel(texture: [string, number]): ICRender.Model;
 }
