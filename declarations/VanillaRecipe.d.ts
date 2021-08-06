ab5ce702346ab3f6b5d1000a8f2ede8523bd9d70
Lapis <45414333+Lapis256@users.noreply.github.com>
2021-08-07 08:20:36 +0900

    Support 1.16. (#5)
    Vanilla Recipe lib update

--- declarations/VanillaRecipe.d.ts
+++ declarations/VanillaRecipe.d.ts
@@ -17,19 +17,20 @@ declare namespace VanillaRecipe {
             count?: number;
         }[];
         result: {
-            item: string,
-            data?: number,
-            count?: number
+            item: string;
+            data?: number;
+            count?: number;
         } | {
-            item: string,
-            data?: number,
-            count?: number
+            item: string;
+            data?: number;
+            count?: number;
         }[];
     };
     export function setResourcePath(path: string): void;
+    export function setBehaviorPath(path: string): void;
     export function getFileName(recipeName: string): string;
     export function getFilePath(recipeName: string): string;
-    export function resetRecipes(): void;
+    export function resetRecipes(path: string): void;
     export function getNumericID(stringID: string): number;
     export function convertToVanillaID(stringID: string): string;
     export function generateJSONRecipe(name: string, obj: any): void;
