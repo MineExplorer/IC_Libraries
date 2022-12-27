dd49edc788cee229878a190aeb247cdba490fd78
MineExplorer <vladgrz40@gmail.com>
2022-12-27 18:00:35 +0300

    Remove BaseBlocks library

--- declarations/BaseBlocks.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="./core-engine.d.ts" />
-
-declare namespace BaseBlocks {
-	function createSlab(stringID: string, variants: Block.BlockVariation[], blockType: string | Block.SpecialType, doubleSlabID: number): void
-	function createDoubleSlab(stringID: string, variants: Block.BlockVariation[], blockType: string | Block.SpecialType, slabID: number): void
-	function addDropOnExplosion(blockID: number): void
-}
