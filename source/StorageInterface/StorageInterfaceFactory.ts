6df05e1673324480323761e47b1dfbf3d6dbf806
MineExplorer <vladgrz40@gmail.com>
2026-08-29 15:03:30 +0300

    make tileEntity arg required in storage interface
    but remain reverse compatibility

--- source/StorageInterface/StorageInterfaceFactory.ts
+++ source/StorageInterface/StorageInterfaceFactory.ts
@@ -6,17 +6,46 @@ namespace StorageInterfaceFactory {
             return tileEntity.__storageInterface;
         }
         const storagePrototype = StorageInterface.getPrototype(tileEntity.blockID);
-        let interface: StorageInterface.TileEntityInterface;
+        let tileInterface: StorageInterface.TileEntityInterface;
         if (storagePrototype) {
-            interface = new storagePrototype.classType(tileEntity)
+            tileInterface = new storagePrototype.classType(tileEntity)
             for (let key in storagePrototype) {
                 if (key == "classType") continue;
-                interface[key] = storagePrototype[key];
+                // Reverse compatibility with callers who do not pass tileEntity arg.
+                if (key == "canReceiveLiquid") {
+                    tileInterface["__canReceiveLiquid"] = storagePrototype[key];
+                    tileInterface[key] = function(liquid, side, tileEntity = this.tileEntity) {
+                        return this.__canReceiveLiquid(liquid, side, tileEntity);
+                    }
+                    continue;
+                }
+                if (key == "canTransportLiquid") {
+                    tileInterface["__canTransportLiquid"] = storagePrototype[key];
+                    tileInterface[key] = function(liquid, side, tileEntity = this.tileEntity) {
+                        return this.__canTransportLiquid(liquid, side, tileEntity);
+                    }
+                    continue;
+                }
+                if (key == "getInputTank") {
+                    tileInterface["__getInputTank"] = storagePrototype[key];
+                    tileInterface[key] = function(side, tileEntity = this.tileEntity) {
+                        return this.__getInputTank(side, tileEntity);
+                    }
+                    continue;
+                }
+                if (key == "getOutputTank") {
+                    tileInterface["__getOutputTank"] = storagePrototype[key];
+                    tileInterface[key] = function(side, tileEntity = this.tileEntity) {
+                        return this.__getOutputTank(side, tileEntity);
+                    }
+                    continue;
+                }
+                tileInterface[key] = storagePrototype[key];
             }
         } else {
-            interface = new StorageInterface.TileEntityInterface(tileEntity);
+            tileInterface = new StorageInterface.TileEntityInterface(tileEntity);
         }
-        tileEntity.__storageInterface = interface;
-        return interface;
+        tileEntity.__storageInterface = tileInterface;
+        return tileInterface;
     }
 }
